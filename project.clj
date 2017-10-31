81b7ae8bc576be9268539c2f9a13dcfa45a537c8
Nurullah Akkaya <nurullah@nakkaya.com>
2017-11-01 00:05:24 +0200

    bump version deploy clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject control-toolbox "0.2.3"
+(defproject control-toolbox "0.2.4"
   :description "Robotics Toolbox"
   :url "https://git.nakkaya.com/nakkaya/control-toolbox/tree/master"
   :license {:name "Eclipse Public License"
